8699a34adae61c77bafad144c07681dfed8358f5
Cthulhu21 <62732060+Cthulhu21@users.noreply.github.com>
2020-05-13 19:29:01 -0500

    Funciones de la interfaz
    Lo de arriba. Más el guardado de las ventas y tablas para que el cliente vea lo que ha pedido

--- interfazclientes.cpp
+++ interfazclientes.cpp
@@ -11,6 +11,7 @@ InterfazClientes::InterfazClientes(QWidget *parent) :
     CargarInventario();
     CargarCombos();
     MostrarTabla();
+    ui->TablaPedido->hide();
 }
 
 InterfazClientes::~InterfazClientes()
@@ -102,7 +103,7 @@ void InterfazClientes::CargarCombos()
     {
         while(!Archivo.eof())
         {
-            string Linea, ID, IDs_Elementos, PrecioTotal;
+            string Linea, ID, IDs_Elementos, PrecioTotal, Cantidad;
             getline(Archivo,Linea);
             int Contador=0;
             for(auto Caracter : Linea)
@@ -130,13 +131,23 @@ void InterfazClientes::CargarCombos()
                     }
                     break;
                 case 2:
+                    if(Caracter==';')
+                    {
+                        Contador++;
+                    }
+                    else
+                    {
+                        Cantidad+=Caracter;
+                    }
+                    break;
+                case 3:
                     PrecioTotal+=Caracter;
                     break;
                 }
             }
             if(ID!="")
             {
-                Combos[ID]={IDs_Elementos,PrecioTotal};
+                Combos[ID]={IDs_Elementos,Cantidad,PrecioTotal};
             }
         }
     }
@@ -145,12 +156,12 @@ void InterfazClientes::CargarCombos()
 void InterfazClientes::MostrarTabla()
 {
     ui->Tabla->show();
-    ui->Tabla->setColumnCount(3);
-    QStringList Cabecera, IDs, Contenido, Precios;
-    Cabecera <<"ID"<<"Contenido"<<"Precio";
+    ui->Tabla->setColumnCount(4);
+    QStringList Cabecera, IDs, Contenido, Precios, Cantidad;
+    Cabecera <<"ID"<<"Contenido"<<"Cantidad"<<"Precio";
     for(auto Item: Combos)
     {
-        QString _IDs,_Contenidos,_Precios;
+        QString _IDs,_Contenidos,_Precios,_Cantidad;
         for(auto Caracter: Item.first)
         {
             _IDs+=Caracter;
@@ -165,6 +176,13 @@ void InterfazClientes::MostrarTabla()
                     _Contenidos+=Caracter;
                 }
             }
+            else if(Contador==1)
+            {
+                for(auto Caracter : Lista)
+                {
+                    _Cantidad+=Caracter;
+                }
+            }
             else
             {
                 for(auto Caracter: Lista)
@@ -177,18 +195,203 @@ void InterfazClientes::MostrarTabla()
         IDs<<_IDs;
         Contenido<<_Contenidos;
         Precios<<_Precios;
+        Cantidad<<_Cantidad;
     }
     ui->Tabla->setHorizontalHeaderLabels(Cabecera);
     int Fila=0;
-    auto ComienzoContenido=Contenido.begin(), ComienzoPrecios=Precios.begin();
+    auto ComienzoContenido=Contenido.begin(), ComienzoPrecios=Precios.begin(), ComienzoCantidad=Cantidad.begin();
     for(auto Elemento: IDs)
     {
         ui->Tabla->insertRow(ui->Tabla->rowCount());
         ui->Tabla->setItem(Fila, 0, new QTableWidgetItem(Elemento));
         ui->Tabla->setItem(Fila, 1, new QTableWidgetItem(*ComienzoContenido));
-        ui->Tabla->setItem(Fila++, 2, new QTableWidgetItem(*ComienzoPrecios));
-        ComienzoPrecios++;
-        ComienzoContenido++;
+        ui->Tabla->setItem(Fila, 2, new QTableWidgetItem(*ComienzoCantidad));
+        ui->Tabla->setItem(Fila++, 3, new QTableWidgetItem(*ComienzoPrecios));
+        ComienzoPrecios++, ComienzoContenido++, ComienzoCantidad++;
     }
 }
 
+
+void InterfazClientes::on_BotonListo_clicked()
+{
+    QString Asiento=ui->TextoAsiento->text();
+    if(PaqueteActual.isEmpty())
+    {
+        QMessageBox::critical(this, "Error", "Ingrese su pedido");
+    }
+    else if(Asiento=="")
+    {
+        QMessageBox::critical(this, "Error", "Ingrese su asiento");
+    }
+    else
+    {
+        QString Mensaje;
+        string Contenido="Debe pagar: "+to_string(PrecioTotal)+" pesos";
+        for(auto Caracter: Contenido)
+        {
+            Mensaje+=Caracter;
+        }
+        QMessageBox::information(this, "Exito", Mensaje);
+        for(auto Elemento: PaqueteActual)
+        {
+            string _Elemento;
+            for(auto Caracter: Elemento)
+            {
+                _Elemento+=Caracter.toLatin1();
+            }
+            Ventas.push_back(_Elemento);
+        }
+        QString Informacion=Asiento+';';
+        for(auto Elemento:PaqueteActual)
+        {
+            Informacion+=Elemento+';';
+        }
+        Clientes.push_back(Informacion);
+        PaqueteActual.clear();
+        ui->TextoAsiento->clear();
+        ui->TablaPedido->clear();
+        ui->TablaPedido->hide();
+        Filas=0;
+        PrecioTotal=0;
+
+    }
+}
+
+void InterfazClientes::on_BotoAgregarCombo_clicked()
+{
+    QString _ID=ui->TextoID->text();
+    string ID;
+    bool Error=false;
+    for(auto Caracter: _ID)
+    {
+        ID+=Caracter.toLatin1();
+    }
+    if(ID=="" or Combos.find(ID)==Combos.end())
+    {
+        Error=true;
+    }
+    if(!Error)
+    {
+        QStringList Cabecera;
+        ui->TablaPedido->setColumnCount(2);
+        Cabecera<<"Pedido"<<"Coste";
+        ui->TablaPedido->setHorizontalHeaderLabels(Cabecera);
+
+        QString Nombre, Precio;
+        list<string> Contenido=Combos[ID];
+        int Contador=0;
+        for(auto Elemento: Contenido)
+        {
+            switch (Contador)
+            {
+            case 0:
+                for(auto Caracter: Elemento)
+                {
+                    Nombre+=Caracter;
+                }
+                break;
+            case 2:
+                PrecioTotal+=stoi(Elemento,nullptr,10);
+                for(auto Caracter:Elemento)
+                {
+                    Precio+=Caracter;
+                }
+            }
+            Contador++;
+        }
+        ui->TablaPedido->insertRow(ui->TablaPedido->rowCount());
+        ui->TablaPedido->setItem(Filas,0,new QTableWidgetItem(Nombre));
+        ui->TablaPedido->setItem(Filas++,1, new QTableWidgetItem(Precio));
+        ui->TablaPedido->show();
+        PaqueteActual.push_back(_ID+";"+Nombre+";"+Precio);
+        ui->TextoID->clear();
+    }
+    else
+    {
+        QMessageBox::critical(this, "Error", "Ingrese un ID válido");
+    }
+}
+
+void InterfazClientes::on_BotonReiniciarPedido_clicked()
+{
+    Filas=0, PrecioTotal=0;;
+    PaqueteActual.clear();
+    ui->TablaPedido->clear();
+    ui->TablaPedido->hide();
+    QMessageBox::information(this, "Reinicio", "Se ha reiniciado su pedido");
+}
+
+
+void InterfazClientes::on_Salir_clicked()
+{
+    GuardarVentas();
+    this->close();
+}
+
+void InterfazClientes::GuardarVentas()
+{
+    fstream Archivo;
+    Archivo.open("Ventas.txt",ios::in);
+    if(Archivo.fail())
+    {
+        QMessageBox::information(this,"Error", "No se pudieron guardar las ventas");
+    }
+    else
+    {
+        string Linea;
+        while(!Archivo.eof())
+        {
+            string ID, Nombre, Precio;
+            getline(Archivo,Linea);
+            int Contador=0;
+            for(auto Caracter: Linea)
+            {
+                switch (Contador)
+                {
+                case 0:
+                    if(Caracter==';')
+                    {
+                        Contador++;
+                    }
+                    else
+                    {
+                        ID+=Caracter;
+                    }
+                    break;
+                case 1:
+                    if(Caracter==';')
+                    {
+                        Contador++;
+                    }
+                    else
+                    {
+                        Nombre+=Caracter;
+                    }
+                case 2:
+                    if(Caracter==';')
+                    {
+                        Contador++;
+                    }
+                    else
+                    {
+                        Precio+=Caracter;
+                    }
+                    break;
+                }
+            }
+            if(ID!="")
+            {
+                Ventas.push_back({ID+";"+Nombre+";"+Precio});
+            }
+        }
+        Archivo.close();
+        ofstream Archivo;
+        Archivo.open("Ventas.txt",ios::out);
+        for(auto Elemento: Ventas)
+        {
+            Archivo<<Elemento;
+        }
+        Archivo.close();
+    }
+
+}
--- interfazclientes.h
+++ interfazclientes.h
@@ -19,14 +19,26 @@ public:
     explicit InterfazClientes(QWidget *parent = nullptr);
     ~InterfazClientes();
 
+private slots:
+    void on_BotonListo_clicked();
+
+    void on_BotoAgregarCombo_clicked();
+
+    void on_BotonReiniciarPedido_clicked();
+
+    void GuardarVentas();
+
+    void on_Salir_clicked();
+
 private:
     Ui::InterfazClientes *ui;
     void CargarInventario();
     void CargarCombos();
     void MostrarTabla();
     list<string> Ventas;
-    QList<QString> PaqueteActual;
+    QList<QString> PaqueteActual, Clientes;
     map<string,list<string>> Inventario,Combos;
+    int Filas=0, PrecioTotal=0;
 };
 
 #endif // INTERFAZCLIENTES_H
